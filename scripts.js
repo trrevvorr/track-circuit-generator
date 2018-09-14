6d3aa46f48229e5e4b8a13c16a0120771348fd16
trrevvorr <t2r1Github>
2018-09-13 21:09:40 -0500

    modified circuit settings
    added number input field for ranges, improved exercise selection logic,
    changed duration to max exercises

--- scripts.js
+++ scripts.js
@@ -1,27 +1,40 @@
-function generateCircuit() {	
-	const maxDifficulty = getMaxDifficulty();
-	const maxDuration = getMaxDuration();
-	var chosenExercises = [];
-	var totalDuration = 0;
-	var totalDifficulty = 0;
-	var exercises = EXERCISES.slice();
-	shuffle(exercises);
+//////// CIRCUIT GENERATION ////////
 
-	while ((totalDuration < maxDuration) && 
-		   (totalDifficulty < maxDifficulty) &&
-		   (exercises.length)) {
-		var randomExecise = exercises.pop();
-		totalDuration += randomExecise.duration;
-		totalDifficulty += randomExecise.difficulty;
-		chosenExercises.push(randomExecise);
+function generateCircuit() {
+	const maxDifficulty = getMaxDifficulty();
+	const exerciseLimit = getExerciseCount();
+	var chosenExercises = [];
+	var totalExercises = 0;
+	var totalDifficulty = 0;
+	var possibleExercises = EXERCISES.slice();
+	shuffle(possibleExercises);
+
+	while (possibleExercises.length &&
+			(chosenExercises.length < exerciseLimit) &&
+			(totalDifficulty < maxDifficulty)) {
+		var randomExecise = possibleExercises.pop();
+		if (validateExercise(randomExecise, totalDifficulty, maxDifficulty)) {
+			totalDifficulty += randomExecise.difficulty;
+			chosenExercises.push(randomExecise);
+		}
 	}
-	
-	loadCircut(chosenExercises);
+
+	setCircut(chosenExercises);
 }
 
-function getMaxDuration() {
-	var duration = document.querySelector(".duration-slider input").value;
-	return parseInt(duration) || 0;
+function validateExercise(exercise, totalDifficulty, maxDifficulty) {
+	var addedDifficulty = parseInt(exercise.difficulty);
+	if (isNaN(addedDifficulty)) {
+		console.log(`ERROR: Exercise "${exercise.title}" has difficulty ${exercise.difficulty}`);
+		addedDifficulty = 0;
+	}
+
+	return (totalDifficulty + addedDifficulty) <= maxDifficulty;
+}
+
+function getExerciseCount() {
+	var exerciseCount = document.querySelector(".exercise-count-slider input").value;
+	return parseInt(exerciseCount) || 0;
 }
 
 function getMaxDifficulty() {
@@ -29,11 +42,11 @@ function getMaxDifficulty() {
 	return parseInt(difficulty) || 0;
 }
 
-function loadCircut(exercises) {
+function setCircut(exercises) {
 	const templateExercise = document.querySelector("#circuit-item-template");
 	const exerciseListNode = document.querySelector(".circuit-list");
 	exerciseListNode.innerHTML = "";
-	
+
 	for (var i in exercises) {
 		var exercise = exercises[i];
 		var exerciseNode = templateExercise.content.cloneNode(true);
@@ -46,6 +59,22 @@ function setExerciseTemplateData(node, exercise) {
 	node.querySelector(".item-title").textContent = exercise.title;
 }
 
+//////// EVENT HANDLERS ////////
+
+function rangeChanged(node) {
+	var rangeRow = node.parentElement;
+	var newValue = node.value;
+	var nodeType = node.type;
+
+	if (nodeType === "number") {
+		var otherRange = rangeRow.querySelector("input[type='range']");
+	} else {
+		var otherRange = rangeRow.querySelector("input[type='number']");
+	}
+
+	otherRange.value = node.value;
+}
+
 function toggleCompletion(exerciseNode, event) {
 	var checkbox = exerciseNode.querySelector("input[type='checkbox']");
 	// toggle check if neccessary
@@ -53,7 +82,7 @@ function toggleCompletion(exerciseNode, event) {
 		checkbox.checked = !checkbox.checked;
 	}
 
-	// apply or remove checked class 
+	// apply or remove checked class
 	if (checkbox.checked) {
 		exerciseNode.classList.add("completed");
 	} else {
@@ -61,13 +90,16 @@ function toggleCompletion(exerciseNode, event) {
 	}
 }
 
-function shuffle(a) {
-    var j, x, i;
-    for (i = a.length - 1; i > 0; i--) {
-        j = Math.floor(Math.random() * (i + 1));
-        x = a[i];
-        a[i] = a[j];
-        a[j] = x;
-    }
-    return a;
+//////// HELPER FUNCTIONS ////////
+
+function shuffle(array) {
+	var randIndex, element;
+	for (var i = array.length - 1; i > 0; i--) {
+		randIndex = Math.floor(Math.random() * (i + 1));
+		element = array[i];
+		array[i] = array[randIndex];
+		array[randIndex] = element;
+	}
+	return array;
 }
+
